76a6d6795512a604c025b494ac634c71832668f7
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-01 11:52:22 +0900

    Added callback related API

--- libdile/include/dile_vdec_direct.h
+++ libdile/include/dile_vdec_direct.h
@@ -3,7 +3,11 @@
 
 int DILE_VDEC_DIRECT_Open(unsigned int fourcc, int width, int height, int tridType, int videoIndex);
 
+int DILE_VDEC_DIRECT_SetCallback(void (*cb)(unsigned long long buffID));
+
 int DILE_VDEC_DIRECT_Play(const void *data, size_t size);
+
+int DILE_VDEC_DIRECT_PlayWithCallback(const void *data, size_t size, unsigned long long buffID);
 
 int DILE_VDEC_DIRECT_Stop();
 
--- libdile/src/dile_vdec_direct.c
+++ libdile/src/dile_vdec_direct.c
@@ -2,7 +2,11 @@
 
 int DILE_VDEC_DIRECT_Open(unsigned int fourcc, int width, int height, int tridType, int videoIndex) { return 0; }
 
+int DILE_VDEC_DIRECT_SetCallback(void (*cb)(unsigned long long buffID)) { return 0; }
+
 int DILE_VDEC_DIRECT_Play(const void *data, size_t size) { return 0; }
+
+int DILE_VDEC_DIRECT_PlayWithCallback(const void *data, size_t size, unsigned long long buffID) { return 0; }
 
 int DILE_VDEC_DIRECT_Stop() { return 0; }
 
